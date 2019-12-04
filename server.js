966597866bd0efa7d811c3b5c7561f14ef51ccb9
Moises <163190@ids.upchiapas.edu.mx>
2019-12-04 17:55:48 -0600

    Descartando el uso de la hora y fecha de llegada del viaje

--- server.js
+++ server.js
@@ -9,3 +9,16 @@ app.get('*',function(req,res){
 });
 
 app.listen(process.env.PORT || 3000);
+
+  
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname+'/dist/buscadorFrontAngular'));
+app.get('*',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/buscadorFrontAngular/index.html'));
+});
+
+app.listen(process.env.PORT || 3000);
